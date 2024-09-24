4570eb2addc2f9e999cc8e27547a10933c22bf74
Mohamed Amine Oueslati <amine.oueslati@aderivatives.com>
2024-09-24 11:16:47 +0100

    init project

---
+++ src/main/java/com/example/logs/LogsAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.logs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogsAggregatorApplication.class, args);
+    }
+
+}
